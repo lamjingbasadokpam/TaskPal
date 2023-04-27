80025c76fe578e68c6df340717a63cff5267d55c
lamjingbasadokpam <66506073+lamjingbasadokpam@users.noreply.github.com>
2023-04-27 12:06:40 +0530

    Add files via upload

---
+++ Index.razor.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http.HttpResults;
+using static System.Net.Mime.MediaTypeNames;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace TakPal.Website.Pages
+{
+  public partial class Index
+  {
+    [Inject]
+    private NavigationManager NavigationManager { get; set; }
+    public void ReloadPage()
+    {
+ 
+    }
+
+  }
+}
